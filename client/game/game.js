33f16ffb54b08418e329b7a4f44709b351976c53
Thomas Hoooper <thomash@tomac.local>
2012-04-28 22:57:24 +0100

    defining with, height, and pixel size in camera

--- client/game/game.js
+++ client/game/game.js
@@ -3,14 +3,8 @@ var yourPlayer;
 (function($) {
     $(document).ready(function() {
 
-        var width = 48;
-        var height = 57;
-        var pixelSize = 8;
-        
         // This objects connects to the server. That's pretty important, huh?!
         var gameTimer = new GameTimer(3);
-        
-        
 
         // This object communicates with the server
         var server = new FakeServer();
@@ -21,9 +15,12 @@ var yourPlayer;
             console.log("Gametime:",gameTimer.getGameTime());
         });
 
+        // Width, Height, Pixel size
+        var camera = new Camera(48, 57, 8, gameTimer);
+
 
         // Scenery is awesome! Let's draw it
-        var sceneryMagicTable = new MagicTable($("#scenery")[0], width, height, pixelSize);
+        var sceneryMagicTable = new MagicTable($("#scenery")[0], camera.width, camera.height, camera.pixelSize);
 
         // Map are drawn in the scenery Table and they talk to the server.
         var map = new Map(server, sceneryMagicTable);
@@ -37,37 +34,30 @@ var yourPlayer;
         // Replace this later with map or some function. Ask sven
         var fakeMap = function(){return ""};
 
-        // The last layer is the background. 
-        var backgroundMagicTable = new MagicTable($("#background")[0], width, height, pixelSize);
 
         // This is you! Yeah!
-        var yourPlayer = new Player(fakeMap, width);
-
+        var yourPlayer = new Player(fakeMap, camera.width);
+        camera.setFocusPlayer(yourPlayer);
+        
         // We store all players in a dedicated list
-        var playerList = new PlayerList(yourPlayer, server, fakeMap);
-
-
-        var camera = new Camera(width, height, gameTimer, yourPlayer);
+        var playerList = new PlayerList(yourPlayer, server, fakeMap);        
 
 
         //var playerMagic = new MagicCanvas(playersElem, width, height);
-        var playerMagicTable = new MagicTable($("#players")[0], width, height, pixelSize);
+        var playerMagicTable = new MagicTable($("#players")[0], camera.width, camera.height, camera.pixelSize);
         var playerRenderer = new PlayerRenderer(playerList, gameTimer, camera, playerMagicTable);
 
-        var backgroundElem = $("#players")[0];
+        var backgroundElem = $("#background")[0];
         //var backgroundRenderer = new BackgroundRenderer(gameTimer, camera, backgroundElem);
 
-        // Fancy background!
-        //var backgroundRenderer = new BackgroundRenderer(gameTimer, camera, playerCanvas);
                 
-        gameTimer.start();
-        yourPlayer.addEvent({t: gameTimer.getGameTime(), x: Math.round(width/2), y: 0, v: 10, dx: 0});
+        yourPlayer.addEvent({t: gameTimer.getGameTime(), x: Math.round(camera.width/2), y: 0, v: 10, dx: 0});
 
         // Start the timer!
         gameTimer.start();
 
         // Initiate your player with the current gametime, position and stuff
-        yourPlayer.addEvent({t: gameTimer.getGameTime(), x: Math.round(width/2), y: 0, v: 20, dx: 0});
+        yourPlayer.addEvent({t: gameTimer.getGameTime(), x: Math.round(camera.width/2), y: 0, v: 20, dx: 0});
         
         // Fake controls!
         var fakeControls = setInterval(function() {
