acd4d84fcd2244abad6dc80c0ce3b1afa7ef282e
Sven Over <svenover@svenover.de>
2012-04-29 11:41:22 +0100

    Add tweet link on player's death

--- client/game/game.js
+++ client/game/game.js
@@ -41,6 +41,7 @@ var yourPlayer;
                     server.sendPlayerDead(playerList.id);
                     // TODO: Remove key handlers
                     $("#game").append('<a href="#" class="message" onclick="window.location.reload()">YOU ARE MASH!</a>')
+                    $("#url").append('<br><a href="http://twitter.com/?status=I+just+scored+'+event.score+'+points+in+%23urmash.+http%3A%2F%2Fu-r-mash.spacedentist.net%2F+%23gamehack">TWEET YOUR SCORE!</a>');
                 }
                 else
                 {
