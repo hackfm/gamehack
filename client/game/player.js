7cae1c560d58977b06cdadab1011f93bdea994f4
Sven Over <svenover@svenover.de>
2012-04-29 09:26:34 +0100

    Fix typo

--- client/game/player.js
+++ client/game/player.js
@@ -245,7 +245,7 @@ Player.prototype.getPosition = function(t) {
         T = inc/vy/this.speed;
     }
 
-    return {x:x, y:y, t:now, v:v, dx:dx, obstacle:obstacle, score:score, graceperiod: now<graceperiod_end};
+    return {x:x, y:y, t:now, v:v, dx:dx, obstacle:obstacle, score:score, graceperiod: now<this.graceperiod_end};
 }
 
 if ((typeof(module) != "undefined"))
